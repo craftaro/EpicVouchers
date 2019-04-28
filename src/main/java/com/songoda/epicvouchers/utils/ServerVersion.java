79ec03290134bd437094f8509bde503bc8dd1386
Brianna <bri@songoda.com>
2019-04-28 18:01:26 -0400

    FIxes

--- src/main/java/com/songoda/epicvouchers/utils/ServerVersion.java
+++ src/main/java/com/songoda/epicvouchers/utils/ServerVersion.java
@@ -12,7 +12,7 @@ public enum ServerVersion {
     V1_11("org.bukkit.craftbukkit.v1_11"),
     V1_12("org.bukkit.craftbukkit.v1_12"),
     V1_13("org.bukkit.craftbukkit.v1_13"),
-    V1_14("org.bukkit.craftbukkit.v1_15");
+    V1_14("org.bukkit.craftbukkit.v1_14");
 
 
     private final String packagePrefix;
