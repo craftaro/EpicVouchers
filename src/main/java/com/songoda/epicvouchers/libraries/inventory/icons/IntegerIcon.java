cb815445e14b760f7c6d10ff776603044619b425
Brianna <bri@songoda.com>
2019-06-03 21:40:18 -0400

    If empty integer make it 0

--- src/main/java/com/songoda/epicvouchers/libraries/inventory/icons/IntegerIcon.java
+++ src/main/java/com/songoda/epicvouchers/libraries/inventory/icons/IntegerIcon.java
@@ -9,6 +9,6 @@ import java.util.function.BiConsumer;
 
 public class IntegerIcon extends StringIcon {
     public IntegerIcon(EpicVouchers instance, String string, int current, BiConsumer<Player, Integer> consumer) {
-        super(instance, Material.IRON_INGOT, string, "" + current, (player, edited) -> consumer.accept(player, Integer.parseInt(edited)), StringUtils::isNumeric);
+        super(instance, Material.IRON_INGOT, string, String.valueOf(current), (player, edited) -> consumer.accept(player, edited.equals("") ? 0 : Integer.parseInt(edited)), StringUtils::isNumeric);
     }
 }
